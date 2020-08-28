8216750dec98ef2d1cbc02a6d6b0cd1296da366f
Hiranya <hiranyagarbhchoudhary@gmail.com>
2020-08-28 15:02:54 +0530

    add light

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-//Scene
+// scene instance
 var scene = new THREE.Scene();
 
-// Camera - FOV, Aspect Ratio, near plane, far plane
+// camera instance - FOV, Aspect Ratio, near plane, far plane
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z = 5;
 
-// Renderer (webgl,css2d,css3d,svg)
+// renderer (webgl,css2d,css3d,svg)
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor("#e5e5e5"); // set basic bg color
 renderer.setSize(window.innerWidth,window.innerHeight);
@@ -17,9 +18,23 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth,window.innerHeight); // set to window dims
     camera.aspect = window.innerWidth / window.innerHeight; // readjust the aspect ratio
 
-    camera.updateProjectMatrix();
+    camera.updateProjectionMatrix();
 })
 
-// redner method on scene and camera
-renderer.render(scene,camera);
+// def shape and material
+var geometry = new THREE.SphereGeometry(1,10,10);
+var material = new THREE.MeshLambertMaterial({color: 0x266CAA});
+// shape + material
+var mesh = new THREE.Mesh(geometry, material);
 
+// add light
+var light = new THREE.PointLight(0XFFFFFF, 1.75, 500);
+// set light x,y,z cordinates
+light.position.set(10,0,25)
+scene.add(light);
+
+// add mesh to the scene
+scene.add(mesh);
+
+// render method on scene and camera
+renderer.render(scene,camera);
