ecbc0d6025692d9b105c8ebb94424012bd9fe85a
Hiranya <hiranyagarbhchoudhary@gmail.com>
2020-08-29 15:20:49 +0530

    improve code readability

--- js/main.js
+++ js/main.js
@@ -7,30 +7,30 @@ var camera, scene, renderer;
 var composer;
 var glitchPass;
 
-init();
+main();
 animate();
 
-function init() {
+function main() {
 
-    //scene
+    // scene instance
     scene = new THREE.Scene();
 
-    // camera
+    // camera instance (OPTIONS: FOV, Aspect Ratio, near plane, far plane)
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
     camera.position.z = 400;
 
-    // renderer
+    // renderer (webgl,css2d,css3d,svg)
     renderer = new THREE.WebGLRenderer({antialias: true});  /* antialias to eliminate img tearing */
     renderer.setPixelRatio(window.devicePixelRatio);        // for high DPI device
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor('#000');
-    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);         // append child to renderer DOM element - create canvar with renderer settings
 
     
     // object
     var texture = new THREE.TextureLoader().load('../logo.png');
 
-    var geometry = new THREE.PlaneBufferGeometry( 150, 50 );                            // plane
+    var geometry = new THREE.PlaneBufferGeometry( 150, 50 );                            // def shape -> plane
     var material = new THREE.MeshBasicMaterial({ map: texture, transparent: true});     // material == texture == img
     var mesh = new THREE.Mesh(geometry, material);                                      // mesh = geometry + material
     
@@ -55,67 +55,7 @@ function init() {
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate);                             // casuses the renderer to draw the scene everytime the screen refreshes at 60hz
     composer.render();
 
-}
-
-/* ============= [ END ] ============= */
-
-/* ==========================
-    below is my learning function - may be ignored
-========================== */
-
-var learn = function() {
-
-    // scene instance
-    var scene = new THREE.Scene();
-
-    // camera instance - FOV, Aspect Ratio, near plane, far plane
-    var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-    camera.position.z = 5;
-
-    // renderer (webgl,css2d,css3d,svg)
-    var renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setClearColor("#e5e5e5"); // set basic bg color
-    renderer.setSize(window.innerWidth,window.innerHeight);
-
-    // append child to renderer DOM element - create canvar with renderer settings
-    document.body.appendChild(renderer.domElement);
-
-    // responsive
-    window.addEventListener('resize', () => {
-        renderer.setSize(window.innerWidth,window.innerHeight); // set to window dims
-        camera.aspect = window.innerWidth / window.innerHeight; // readjust the aspect ratio
-
-        camera.updateProjectionMatrix();
-    })
-
-    // add sphere
-    // def shape and material
-
-    var sphere_geometry = new THREE.SphereGeometry(1,50,50); // shape
-    var sp_material = new THREE.MeshLambertMaterial({color: 0x266CAA}); // material
-    var sp_mesh = new THREE.Mesh(sphere_geometry, sp_material); // shape + material
-
-    // add box
-    var box_geometry = new THREE.BoxGeometry(1,1,1);
-    var box_mesh = new THREE.Mesh(box_geometry, sp_material);
-
-    // add light
-    var light = new THREE.PointLight(0XFFFFFF, 1.75, 500);
-    light.position.set(10,0,25) // set light x,y,z cordinates
-
-    // add mesh and light to the scene
-    scene.add(box_mesh, light);
-
-    // render funct on scene and camera
-    var render = function() {
-        requestAnimationFrame(render); // casuses the renderer to draw the scene everytime the screen refreshes at 60hz
-        renderer.render(scene,camera);
-
-        //box_mesh.rotation.x += 0.05;
-        //box_mesh.scale.x += 0.001;
-    }
-    render();
 }
